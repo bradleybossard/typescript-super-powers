730bb1e9b1f1a0ca92174374899405c618696338
Bradley Bossard <bradleybossard@gmail.com>
2018-05-16 23:59:50 -0500

    Function example

--- typescript.js
+++ typescript.js
@@ -1,3 +1,8 @@
 "use strict";
 var messyArray = ['something', 2, true, undefined, null];
 var tuple = [24, 'what', 'e'];
+function sayMyName(name) {
+    return name;
+}
+// sayMyName(32);  // error
+console.log(sayMyName('heather'));
--- typescript.ts
+++ typescript.ts
@@ -1,3 +1,11 @@
 const messyArray = ['something', 2, true, undefined, null];
 
 const tuple: [number, string, string] = [24, 'what', 'e'];
+
+function sayMyName(name: string): string {
+  return name;
+}
+
+// sayMyName(32);  // error
+console.log(sayMyName('heather'));
+
